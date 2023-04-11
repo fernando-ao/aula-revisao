2e1507461bd0522c295e1528af21e44dc154e9f7
Fernando Augusto Ortolan <fernando.ao2000@aluno.ifsc.edu.br>
2023-04-10 21:05:56 -0300

    Add root route

--- routes.js
+++ routes.js
@@ -2,3 +2,26 @@ const express = require("express");
 const routes = express.Router();
 
 module.exports = routes;
+
+
+const list = [
+  {
+    "id": 1,
+    "fname": "João", 
+    "age": 20
+  }, 
+  {
+    "id": 2,
+    "fname": "Fernando", 
+    "age": 22
+  }, 
+  {
+    "id": 3,
+    "fname": "Maria", 
+    "age": 21
+  }
+];
+
+routes.get("/", (req, res) => {
+  res.status(200).json(list);
+});
